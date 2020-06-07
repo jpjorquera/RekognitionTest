dc73e15ea35c8245fb0c514641584bf0df838f94
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2020-06-07 19:57:30 -0400

    agregada prueba base

---
+++ identificar.py
@@ -0,0 +1,76 @@
+#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
+
+import boto3
+
+"""
+def detect_labels(photo, bucket):
+
+    client=boto3.client('rekognition')#, region_name="us-east")
+
+    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
+        MaxLabels=10)
+
+    print('Detected labels for ' + photo) 
+    print()   
+    for label in response['Labels']:
+        print ("Label: " + label['Name'])
+        print ("Confidence: " + str(label['Confidence']))
+        print ("Instances:")
+        for instance in label['Instances']:
+            print ("  Bounding box")
+            print ("    Top: " + str(instance['BoundingBox']['Top']))
+            print ("    Left: " + str(instance['BoundingBox']['Left']))
+            print ("    Width: " +  str(instance['BoundingBox']['Width']))
+            print ("    Height: " +  str(instance['BoundingBox']['Height']))
+            print ("  Confidence: " + str(instance['Confidence']))
+            print()
+
+        print ("Parents:")
+        for parent in label['Parents']:
+            print ("   " + parent['Name'])
+        print ("----------")
+        print ()
+    return len(response['Labels'])
+
+
+def main():
+    photo='default_amazon.png'
+    bucket='rekogtestusm'
+    label_count=detect_labels(photo, bucket)
+    print("Labels detected: " + str(label_count))
+
+
+if __name__ == "__main__":
+    main()
+"""
+
+def detect_text(photo, bucket):
+
+    client=boto3.client('rekognition')
+
+    response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
+                        
+    textDetections=response['TextDetections']
+    print ('Detected text\n----------')
+    for text in textDetections:
+            print ('Detected text:' + text['DetectedText'])
+            print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
+            print ('Id: {}'.format(text['Id']))
+            if 'ParentId' in text:
+                print ('Parent Id: {}'.format(text['ParentId']))
+            print ('Type:' + text['Type'])
+            print()
+    return len(textDetections)
+
+def main():
+
+    photo='default_amazon.png'
+    bucket='rekogtestusm'
+
+    text_count=detect_text(photo,bucket)
+    print("Text detected: " + str(text_count))
+
+
+if __name__ == "__main__":
+    main()
