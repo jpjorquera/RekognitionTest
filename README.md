fa8b58d06a8e92be8cd48be91dc3c12e8e1af6e3
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2020-06-08 09:27:14 -0400

    agregadas carpetas de prueba, filtrado de texto

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Función para comparar texto de una imagen
 La comparación se hace en base a una imagen de control
+
+# Instalar boto3
+pip install boto3
--- identificar.py
+++ identificar.py
@@ -1,75 +1,37 @@
-#Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#PDX-License-Identifier: MIT-0 (For details, see https://github.com/awsdocs/amazon-rekognition-developer-guide/blob/master/LICENSE-SAMPLECODE.)
-
+# librería requerida por aws cli
 import boto3
 
-"""
-def detect_labels(photo, bucket):
+# Filtrar atributos importantes con texto y confianza > 97%
+CONFIDENCE = 97
+def filtrarTexto(textDetections):
+    newTextDetections = []
+    # Ver en cada objeto detectado
+    for text in textDetections:
+        # Filtrar parámetros
+        if text['Type'] == "WORD":
+            if (text['Confidence'] > CONFIDENCE):
+                texto = text['DetectedText']
+                newTextDetections.append(texto)
+    return newTextDetections
 
-    client=boto3.client('rekognition')#, region_name="us-east")
-
-    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}},
-        MaxLabels=10)
-
-    print('Detected labels for ' + photo) 
-    print()   
-    for label in response['Labels']:
-        print ("Label: " + label['Name'])
-        print ("Confidence: " + str(label['Confidence']))
-        print ("Instances:")
-        for instance in label['Instances']:
-            print ("  Bounding box")
-            print ("    Top: " + str(instance['BoundingBox']['Top']))
-            print ("    Left: " + str(instance['BoundingBox']['Left']))
-            print ("    Width: " +  str(instance['BoundingBox']['Width']))
-            print ("    Height: " +  str(instance['BoundingBox']['Height']))
-            print ("  Confidence: " + str(instance['Confidence']))
-            print()
-
-        print ("Parents:")
-        for parent in label['Parents']:
-            print ("   " + parent['Name'])
-        print ("----------")
-        print ()
-    return len(response['Labels'])
-
-
-def main():
-    photo='default_amazon.png'
-    bucket='rekogtestusm'
-    label_count=detect_labels(photo, bucket)
-    print("Labels detected: " + str(label_count))
-
-
-if __name__ == "__main__":
-    main()
-"""
 
 def detect_text(photo, bucket):
-
+    # Hacer request en aws bucket
     client=boto3.client('rekognition')
-
     response=client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
-                        
+    # Extraer respuestas y filtrar
     textDetections=response['TextDetections']
-    print ('Detected text\n----------')
-    for text in textDetections:
-            print ('Detected text:' + text['DetectedText'])
-            print ('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
-            print ('Id: {}'.format(text['Id']))
-            if 'ParentId' in text:
-                print ('Parent Id: {}'.format(text['ParentId']))
-            print ('Type:' + text['Type'])
-            print()
-    return len(textDetections)
+    texto = filtrarTexto(textDetections)
+    return texto
 
 def main():
 
-    photo='default_amazon.png'
+    photo='control.png'
+    #photo='prueba.jpg'
     bucket='rekogtestusm'
 
-    text_count=detect_text(photo,bucket)
-    print("Text detected: " + str(text_count))
+    texto=detect_text(photo,bucket)
+    print("Text detected: " + str(texto))
 
 
 if __name__ == "__main__":
