063b3de9f032c522ad84e7c02ef482cdf2297450
Juan Pablo Jorquera <jp.jorqueraz@gmail.com>
2020-06-08 09:40:41 -0400

    estandarizado texto identificado

--- identificar.py
+++ identificar.py
@@ -1,8 +1,11 @@
 # librería requerida por aws cli
 import boto3
 
-# Filtrar atributos importantes con texto y confianza > 97%
+# Parámetros
+NOMBRE_BUCKET = "rekogtestusm"
 CONFIDENCE = 97
+
+# Filtrar texto de otros atributos y con confianza > 97%
 def filtrarTexto(textDetections):
     newTextDetections = []
     # Ver en cada objeto detectado
@@ -11,10 +14,13 @@ def filtrarTexto(textDetections):
         if text['Type'] == "WORD":
             if (text['Confidence'] > CONFIDENCE):
                 texto = text['DetectedText']
+                # Estandarizar
+                texto = str.lower(texto).strip()
                 newTextDetections.append(texto)
     return newTextDetections
 
-
+# Buscar y aplicar rekognition a imagen en bucket, luego filtrar
+# las respuestas
 def detect_text(photo, bucket):
     # Hacer request en aws bucket
     client=boto3.client('rekognition')
@@ -28,9 +34,9 @@ def main():
 
     photo='control.png'
     #photo='prueba.jpg'
-    bucket='rekogtestusm'
+    bucket=NOMBRE_BUCKET
 
-    texto=detect_text(photo,bucket)
+    texto=detect_text(photo, bucket)
     print("Text detected: " + str(texto))
 
 
